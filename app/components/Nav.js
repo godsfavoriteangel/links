dbf9e7f77087fb82ead36d3ed40328aec9a3de4d
ham ⋆˙⟡♡ <133094508+harmonyeva@users.noreply.github.com>
2024-08-19 18:53:22 -0700

    Mobile Optimization

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -3,19 +3,20 @@ import Link from 'next/link'
 
 const Nav = () => {
     return (
-        <div>
-        <ul id='nav'className='flex flex-row justify-center my-4'>
-          <li>
-            <Link href="/" className='text-pink-200 text-2xl text-shadow-[0_4px_12px_#fbcfe8] px-6 custom-cursor ' >home</Link>
+      <div>
+        <ul id='nav' className='flex flex-row justify-center my-4 xl:w-[1200px] xl:mx-auto'>
+          <li className='flex-1 text-right'>
+            <Link href="/" className='text-pink-200 text-lg md:text-2xl text-shadow-[0_4px_12px_#fbcfe8] px-6 custom-cursor'>home</Link>
           </li>
-          <li>
-            <Link href="/images" className='text-pink-200 text-2xl text-shadow-[0_4px_12px_#fbcfe8] px-6 custom-cursor' >⋆｡‧˚ʚ ୨୧ ɞ˚‧｡⋆</Link>
+          <li className='flex-1 text-center'>
+            <Link href="/images" className='text-pink-200 text-lg md:text-2xl text-shadow-[0_4px_12px_#fbcfe8] custom-cursor'>⋆ ｡‧˚ʚ ୨୧ ɞ˚‧｡⋆</Link>
           </li>
-          <li>
-            <Link href="/coding" className='text-pink-200 text-2xl text-shadow-[0_4px_12px_#fbcfe8] px-6 custom-cursor' >projects</Link>
+          <li className='flex-1 text-left'>
+            <Link href="/coding" className='text-pink-200 text-lg md:text-2xl text-shadow-[0_4px_12px_#fbcfe8] px-6 custom-cursor'>projects</Link>
           </li>
         </ul>
-      </div>
+    </div>
+    
     );
 };
 
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+import pfp from '../public/pfp.png';
 import useSWR from 'swr';
 import { SiSpotify } from 'react-icons/si';
 import Nav from './components/Nav';
@@ -15,26 +16,25 @@ const Home = () => {
   return (
     <div> 
       <Nav />
-      <div id='home_main' className='w-[1100px] border-2 rounded-xl border-pink-200 shadow-[0_0_2px_#fbcfe8,inset_0_0_2px_#fbcfe8,0_0_5px_#fbcfe8,0_0_15px_#fbcfe8,0_0_30px_#fbcfe8] mx-auto mt-10 mb-10 justify-center items-center py-5 px-6 flex flex-row sm:w-[640px] md:w-[768px] lg:w-[1100px] ' >
-          <div id='home_main_profile' className='flex flex-row items-center'>
+      <div id='home_main' className='w-[375px] md:w-[768px] xl:w-[1100px] xl:border-2 xl:rounded-xl xl:border-pink-200 xl:shadow-[0_0_2px_#fbcfe8,inset_0_0_2px_#fbcfe8,0_0_5px_#fbcfe8,0_0_15px_#fbcfe8,0_0_30px_#fbcfe8] mx-auto xl:mt-10 xl:mb-10 justify-center items-center px-6 xl:py-5 flex flex-col xl:flex-row ' >
+          <div id='home_main_profile' className='flex flex-col items-center xl:flex-row'>
           <Image 
             id='home_main_profile_img'
-            src='/pfp.png' 
+            src={pfp} 
             alt='profile' 
-            width={250}
-            height={250}
-            className='w-[250px] h-[250px] rounded-full md:w-[150px] md:h-[150px] lg:w-[250px] lg:h-[250px]'
+            className='w-[200px] h-[200px] md:w-[280px] md:h-[280px] xl:w-[250px] xl:h-[250px] rounded-full'
+            priority
           />
           <div id='home_main_profile_text' className='mx-7'>
-            <h1 id='home_main_profile_title' className='text-pink-200 text-5xl text-shadow-[0_2px_12px_#eec3d6] font-porkys text-center'>
+            <h1 id='home_main_profile_title' className='text-pink-200 text-4xl xl:text-5xl text-shadow-[0_2px_12px_#eec3d6] font-porkys text-center my-2 '>
               ❤︎ h a r m o n y ❤︎
             </h1>
-            <h2 id='home_main_profile_desc' className='text-pink-200 text-shadow-[0_2px_8px_#fbcfe8] w-[400px] mx-2 my-3 text-center font-rainyhearts text-xl/6'>
+            <h2 id='home_main_profile_desc' className='text-pink-200 text-shadow-[0_2px_8px_#fbcfe8] w-[350px] md:w-[400px] mx-2 my-3 text-center font-rainyhearts text-lg/6 md:text-xl/6 '>
               21y/o based in LA  ☆ self taught web dev <br></br> So if on advanced addition absolute received replying throwing he. Delighted consisted newspaper of unfeeling as neglected so. Tell size come hard mrs and four fond a
             </h2>
           </div>
         </div>
-        <div id='home_main_spotify' className=''>
+        <div id='home_main_spotify' className='my-3 xl:my-0'>
           <div id='home_main_spotify_top' className=' rounded-lg border border-pink-200  w-[280px] flex flex-col  items-center pt-2 pb-2 shadow-[0_0_2px_#fbcfe8,inset_0_0_2px_#fbcfe8,0_0_5px_#fbcfe8,0_0_15px_#fbcfe8,0_0_1px_#fbcfe8] '>
             <p id='home_main_spotify_title' className='text-pink-200 text-shadow-[0_2px_8px_#fbcfe8] '>
             ♫ ₊˚. currently listening .˚₊ ♫
